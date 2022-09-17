657fa07abc10ebf549633842e85a3fafe92e4dee
katec23 <katecalcott@hotmail.com>
2022-09-18 09:23:02 +1000

    Fixed typo in JS getForecast function

--- src/index.js
+++ src/index.js
@@ -121,7 +121,7 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "d1a86552de255334f6117b348c4519bd";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axio.get(apiUrl).then(displayForecast);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemperature(response) {
